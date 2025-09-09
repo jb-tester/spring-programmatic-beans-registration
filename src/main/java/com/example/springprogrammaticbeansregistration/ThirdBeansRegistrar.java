48658cedd45e22b2cd611ec719c67cb00df921c5
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2025-09-09 23:45:42 +0200

    comments for further requests

--- src/main/java/com/example/springprogrammaticbeansregistration/ThirdBeansRegistrar.java
+++ src/main/java/com/example/springprogrammaticbeansregistration/ThirdBeansRegistrar.java
@@ -7,19 +7,19 @@ import org.springframework.beans.factory.BeanRegistry;
 import org.springframework.core.env.Environment;
 
 public class ThirdBeansRegistrar implements BeanRegistrar {
-
+// observe the beans diagram: the dependent beans are shown incorrectly
 	@Override
 	public void register(BeanRegistry registry, Environment environment) {
 
 		registry.registerBean("four", BeanFour.class
-		, customizer -> customizer.supplier(context -> new BeanFour(context.bean("firstFoo", Foo.class))));
+		, customizer -> customizer.supplier(context -> new BeanFour(context.bean("firstFoo", Foo.class))));// would be nice to have bean name completion here
 		registry.registerBean("eight1", BeanEight.class);
 		registry.registerBean("eight2", BeanEight.class);
 		registry.registerBean("five", BeanFive.class,
 				customizer -> {
 					customizer.supplier(context -> new BeanFive(
 							context.bean(BeanFour.class),
-							context.bean("eight1", BeanEight.class)));
+							context.bean("eight1", BeanEight.class))); // would be nice to have bean name completion here
 				});
 
 	}
